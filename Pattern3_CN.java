2531913b70ff58367e8e0fdd28bcba7eb788b72d
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-03-28 18:26:32 +0530

    Create Pattern3_CN.java
    1
    1 2 
    1 2 3

---
+++ Pattern3_CN.java
@@ -0,0 +1,51 @@
+/*Problem statement
+Sam is making a Triangular painting for a maths project.
+
+An N-dimensional Triangle is represented by the lower triangle of the pattern filled with integers starting from 1.
+
+For every value of ‘N’, help sam to print the corresponding N-dimensional Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+1
+1 2 
+1 2 3
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1  <= N <= 25
+Time Limit: 1 sec
+Sample Input 1:
+3
+Sample Output 1:
+1
+1 2 
+1 2 3
+Sample Input 2 :
+1
+Sample Output 2 :
+1*/
+==================================================================================================================
+  =========================================================================================================
+  public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+           for (int i = 1; i <= n; i++)
+    {
+        // This is the inner loop which loops for the columns
+       // no. of columns = row number for each line here.
+       // Here, we print numbers from 1 to the row number
+       // instead of stars in each row.
+        for (int j = 1; j <= i; j++)
+        {
+            System.out.print(j+" ");
+        }
+
+         // As soon as numbers for each iteration are printed, we move to the
+        // next row and give a line break otherwise all numbers
+        // would get printed in 1 line.
+        System.out.println();
+    }
+    }
+}
