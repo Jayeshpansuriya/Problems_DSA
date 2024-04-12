3ff382bccaa7e3797cd8a25b2e01c32f92f466bb
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-04-12 06:08:33 +0530

    Create Find the Largest element in an array_CN.java
    Find the Largest element in an array using brute force aproch

---
+++ Find the Largest element in an array_CN.java
@@ -0,0 +1,35 @@
+/*
+Brute Force Aproch 
+Approach: 
+Sort the array in ascending order.
+Print the (size of the array -1)th index.
+*/
+
+DryRun: 
+Before sorting: arr[] = {2,5,1,3,0};
+
+After sorting: arr[] = {0,1,2,3,5};
+
+Hence answer : arr[sizeofthearray-1] =5
+
+===============================================================================================================
+  =============================================================================================================
+
+  
+import java.util.*;
+public class jd {
+ 
+  public static void main(String args[]) {
+ 
+    int arr1[] =  {2,5,1,3,0};
+    System.out.println("The Largest element in the array is: " + sort(arr1));
+   
+    int arr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: " + sort(arr2));
+  }
+  static int sort(int arr[]) {
+    Arrays.sort(arr);
+    return arr[arr.length - 1];
+  }
+}
+
