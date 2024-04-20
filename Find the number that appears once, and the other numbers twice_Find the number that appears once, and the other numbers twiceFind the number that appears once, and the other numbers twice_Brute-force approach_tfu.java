121c66d274eda072f4b9a53a7ddbd29c3e4fd322
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-04-20 18:00:59 +0530

    Create Find the number that appears once, and the other numbers twice_Find the number that appears once, and the other numbers twiceFind the number that appears once, and the other numbers twice_Brute-force approach_tfu.java
     Given a non-empty array of integers arr, every element appears twice except for one. Find that single one. using Bruth forcs aproch

---
+++ Find the number that appears once, and the other numbers twice_Find the number that appears once, and the other numbers twiceFind the number that appears once, and the other numbers twice_Brute-force approach_tfu.java
@@ -0,0 +1,77 @@
+/*
+Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+
+Examples
+Example 1:
+Input Format:
+ arr[] = {2,2,1}
+Result:
+ 1
+Explanation:
+ In this array, only the element 1 appear once and so it is the answer.
+
+Example 2:
+Input Format:
+ arr[] = {4,1,2,1,2}
+Result:
+ 4
+Explanation:
+ In this array, only element 4 appear once and the other elements appear twice. So, 4 is the answer.
+
+ 
+*/
+=======================================================================================================================================
+  /*
+  Intuition:
+For every element present in the array, we will do a linear search and count the occurrence. If for any element, the occurrence is 1, we will return it.
+
+Approach:
+The steps are as follows:
+
+First, we will run a loop(say i) to select the elements of the array one by one.
+For every element arr[i], we will perform a linear search using another loop and count its occurrence.
+If for any element the occurrence is 1, we will return it.
+
+Time Complexity: O(N2), where N = size of the given array.
+Reason: For every element, we are performing a linear search to count its occurrence. The linear search takes O(N) time complexity. And there are N elements in the array. So, the total time complexity will be O(N2).
+
+Space Complexity: O(1) as we are not using any extra space.
+  */
+  
+
+
+import java.util.*;
+
+public class jd {
+    public static int getSingleElement(int []arr) {
+        // Size of the array:
+        int n = arr.length;
+
+        //Run a loop for selecting elements:
+        for (int i = 0; i < n; i++) {
+            int num = arr[i]; // selected element
+            int cnt = 0;
+
+            //find the occurrence using linear search:
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == num)
+                    cnt++;
+            }
+
+            // if the occurrence is 1 return ans:
+            if (cnt == 1) return num;
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
+
