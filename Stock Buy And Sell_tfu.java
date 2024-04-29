2897d3da8fe69354b55a381af5ccb4205a9986df
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-04-30 05:09:03 +0530

    Create Stock Buy And Sell_tfu.java
    Intuition: We can simply use 2 loops and track every transaction and maintain a variable maxPro to contain the max value among all transactions.

---
+++ Stock Buy And Sell_tfu.java
@@ -0,0 +1,74 @@
+/*
+Stock Buy And Sell
+
+
+27
+
+1
+Problem Statement: You are given an array of prices where prices[i] is the price of a given stock on an ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Examples
+Example 1:
+Input:
+ prices = [7,1,5,3,6,4]
+Output:
+ 5
+Explanation:
+ Buy on day 2 (price = 1) and 
+sell on day 5 (price = 6), profit = 6-1 = 5.
+
+Note
+: That buying on day 2 and selling on day 1 
+is not allowed because you must buy before 
+you sell.
+
+Example 2:
+Input:
+ prices = [7,6,4,3,1]
+Output:
+ 0
+Explanation:
+ In this case, no transactions are 
+done and the max profit = 0.
+*/
+==================================================================================================================================
+  /*
+Intuition: We can simply use 2 loops and track every transaction and maintain a variable maxPro to contain the max value among all transactions.
+
+Approach: 
+
+Use a for loop of ‘i’ from 0 to n.
+Use another for loop of j from ‘i+1’ to n.
+If arr[j] > arr[i] , take the difference and compare  and store it in the maxPro variable.
+Return maxPro.
+
+  */
+  
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int maxPro = maxProfit(arr);
+        System.out.println("Max profit is: " + maxPro);
+    }
+
+    static int maxProfit(int[] arr) {
+        int maxPro = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    maxPro = Math.max(arr[j] - arr[i], maxPro);
+                }
+            }
+        }
+        return maxPro;
+    }
+}
+
+/*
+Time complexity: O(n^2)
+
+Space Complexity: O(1)
+*/
