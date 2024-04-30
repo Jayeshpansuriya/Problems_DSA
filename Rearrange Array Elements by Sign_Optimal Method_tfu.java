48743af978a7907e9c43edffec4d2fcec52a24b3
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-04-30 07:43:51 +0530

    Create Rearrange Array Elements by Sign_Optimal Method_tfu.java
    In this optimal approach, we will try to solve the problem in a single pass and try to arrange the array elements in the correct order in that pass only.
    We know that the resultant array must start from a positive element so we initialize the positive index as 0 and negative index as 1 and start traversing the array such that whenever we see the first positive element, it occupies the space at 0 and then posIndex increases by 2 (alternate places).
    Similarly, when we encounter the first negative element, it occupies the position at index 1, and then each time we find a negative number, we put it on the negIndex and it increments by 2.
    When both the negIndex and posIndex exceed the size of the array, we see that the whole array is now rearranged alternatively according to the sign.

---
+++ Rearrange Array Elements by Sign_Optimal Method_tfu.java
@@ -0,0 +1,96 @@
+/*
+Problem Statement:
+
+There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
+
+Note: Start the array with positive elements.
+
+Examples: 
+
+Example 1:
+
+Input:
+arr[] = {1,2,-4,-5}, N = 4
+Output:
+1 -4 2 -5
+
+Explanation: 
+
+Positive elements = 1,2
+Negative elements = -4,-5
+To maintain relative ordering, 1 must occur before 2, and -4 must occur before -5.
+
+Example 2:
+Input:
+arr[] = {1,2,-3,-1,-2,-3}, N = 6
+Output:
+1 -3 2 -1 3 -2
+Explanation: 
+
+Positive elements = 1,2,3
+Negative elements = -3,-1,-2
+To maintain relative ordering, 1 must occur before 2, and 2 must occur before 3.
+Also, -3 should come before -1, and -1 should come before -2.
+*/
+
+==================================================================================================================================
+/*  Optimal Method
+
+Approach:
+
+In this optimal approach, we will try to solve the problem in a single pass and try to arrange the array elements in the correct order in that pass only.
+We know that the resultant array must start from a positive element so we initialize the positive index as 0 and negative index as 1 and start traversing the array such that whenever we see the first positive element, it occupies the space at 0 and then posIndex increases by 2 (alternate places).
+Similarly, when we encounter the first negative element, it occupies the position at index 1, and then each time we find a negative number, we put it on the negIndex and it increments by 2.
+When both the negIndex and posIndex exceed the size of the array, we see that the whole array is now rearranged alternatively according to the sign.
+
+
+  */
+
+  import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array Initialization.
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, -4, -5));
+        ArrayList<Integer> ans = RearrangebySign(A);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    public static ArrayList<Integer> RearrangebySign(ArrayList<Integer> A) {
+        int n = A.size();
+
+        // Define array for storing the ans separately.
+        ArrayList<Integer> ans = new ArrayList<>(Collections.nCopies(n, 0));
+
+        // positive elements start from 0 and negative from 1.
+        int posIndex = 0, negIndex = 1;
+        for (int i = 0; i < n; i++) {
+
+            // Fill negative elements in odd indices and inc by 2.
+            if (A.get(i) < 0) {
+                ans.set(negIndex, A.get(i));
+                negIndex += 2;
+            }
+
+            // Fill positive elements in even indices and inc by 2.
+            else {
+                ans.set(posIndex, A.get(i));
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+}
+
+
+/*
+Time Complexity: O(N) { O(N) for traversing the array once and substituting positives and negatives simultaneously using pointers, where N = size of the array A}.
+
+Space Complexity:  O(N) { Extra Space used to store the rearranged elements separately in an array, where N = size of array A}.
+
+
+*/
