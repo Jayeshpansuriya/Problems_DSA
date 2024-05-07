f145d3e8b015e67fcf8d00a2c186c0dfd5769af4
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-05-07 18:40:09 +0530

    Create Pascal's Triangle_Variation 3_Naive Approach_tfu.java
    Variation 3: Given the number of rows n. Print the first n rows of Pascal’s triangle

---
+++ Pascal's Triangle_Variation 3_Naive Approach_tfu.java
@@ -0,0 +1,62 @@
+/*
+Optimal Approach
+Algorithm / Intuition
+Now, in the optimal approach of variation 2, we have learned how to generate a row in O(n) time complexity. So, in order to optimize the overall time complexity, we will be using that approach for every row. Thus the total time complexity will reduce.
+
+Approach:
+The steps are as follows:
+
+First, we will run a loop(say row) from 1 to n.
+Inside the loop, we will call a generateRow() function and add the returned list to our final answer. Inside the function we will do the following:
+First, we will store the 1st element i.e. 1 manually.
+After that, we will use a loop(say col) that runs from 1 to n-1. It will store the rest of the elements.
+Inside the loop, we will use the specified formula to print the element. We will multiply the previous answer by (row-col) and then divide it by col itself.
+Thus, the entire row will be stored and returned.
+Finally, we will return the answer list.
+*/
+
+import java.util.*;
+
+public class tUf {
+    public static List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1); //inserting the 1st element
+
+        //calculate the rest of the elements:
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+
+    public static List<List<Integer>> pascalTriangle(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        //store the entire pascal's triangle:
+        for (int row = 1; row <= n; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        List<List<Integer>> ans = pascalTriangle(n);
+        for (List<Integer> it : ans) {
+            for (int ele : it) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Complexity Analysis
+Time Complexity: O(n2), where n = number of rows(given).
+Reason: We are generating a row for each single row. The number of rows is n. And generating an entire row takes O(n) time complexity.
+
+Space Complexity: In this case, we are only using space to store the answer. That is why space complexity can still be considered as O(1).
+*/
