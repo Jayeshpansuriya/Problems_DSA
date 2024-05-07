aa4ca0b2746b373a7e8b5d54376bf4df4e108442
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-05-07 18:36:11 +0530

    Create Pascal's Triangle_Variation 2_tfu.java
    Variation 2: Given the row number n. Print the n-th row of Pascal’s triangle.

---
+++ Pascal's Triangle_Variation 2_tfu.java
@@ -0,0 +1,60 @@
+/*
+Given the row number n. Print the n-th row of Pascal’s triangle.
+
+Our first observation regarding Pascal’s triangle should be that the n-th row of the triangle has exactly n elements. With this observation, we will proceed to solve this problem.
+
+Naive Approach
+In this approach, for every column from 1 to n, we will calculate the element (n, c)(where n is the given row number and c is the column number that will vary from 1 to n) using the previous method. Thus, we will print the row.  
+
+Algorithm / Intuition
+In this approach, for every column from 1 to n, we will calculate the element (n, c)(where n is the given row number and c is the column number that will vary from 1 to n) using the previous method. Thus, we will print the row.  
+
+Approach:
+The steps are as follows:
+
+We will use a loop(say c) to iterate over each column i.e. from 1 to n. And for each column, we will do the following steps:
+First, we will consider n-1 as n and c-1 as r.
+After that, we will simply calculate the value of the combination using a loop. 
+The loop will run from 0 to r. And in each iteration, we will multiply (n-i) with the result and divide the result by (i+1).
+Finally, we will print the element.
+Finally, the entire row will be printed.
+*/
+
+
+
+import java.util.*;
+
+public class Main {
+
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static void pascalTriangle(int n) {
+        // printing the entire row n:
+        for (int c = 1; c <= n; c++) {
+            System.out.print(nCr(n - 1, c - 1) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pascalTriangle(n);
+    }
+}
+
+/*
+Complexity Analysis
+Time Complexity: O(n*r), where n is the given row number, and r is the column index which can vary from 0 to n-1.
+Reason: We are calculating the element for each column. Now, there are total n columns, and for each column, the calculation of the element takes O(r) time where r is the column index.
+
+Space Complexity: O(1) as we are not using any extra space.
+*/
