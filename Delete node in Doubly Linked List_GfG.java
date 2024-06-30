8892d51e5bba51c07e8d36f5bd3f5be1d417faa3
Jayesh Dineshbhai Pansuriya <133566987+Jayeshpansuriya@users.noreply.github.com>
2024-06-30 11:17:10 +0530

    Create Delete node in Doubly Linked List_GfG.java
    Given a doubly Linked list and a position. The task is to delete a node from a given position (position starts from 1) in a doubly linked list and return the head of the doubly Linked list.

---
+++ Delete node in Doubly Linked List_GfG.java
@@ -0,0 +1,61 @@
+/*
+Given a doubly Linked list and a position. The task is to delete a node from a given position (position starts from 1) in a doubly linked list and return the head of the doubly Linked list.
+
+Examples:
+
+Input: LinkedList = 1 <--> 3 <--> 4, x = 3
+Output: 1 3  
+Explanation: 
+After deleting the node at position 3 (position starts from 1),the linked list will be now as 1 <--> 3.
+ 
+Input: LinkedList = 1 <--> 5 <--> 2 <--> 9, x = 1
+Output: 5 2 9
+Explanation:
+
+Expected Time Complexity: O(n)
+Expected Auxilliary Space: O(1)
+
+Constraints:
+2 <= size of the linked list(n) <= 105
+1 <= x <= n
+1 <= node.data <= 109
+*/
+
+/*
+
+Definition for doubly Link List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        
+        if(x==1) return head.next;
+        
+        Node temp=head;
+        
+        for(int i=1;i<x-1;i++){
+            temp=temp.next;
+            
+        }
+        
+        Node temp1=temp.next;
+        temp.next=temp1.next;
+        
+        if(temp1.next!=null) temp1.next.prev=temp;
+        
+        return head;
+    }
+    
+    
+    
+}
